682fd5683dcaec118554a1d485eed8bb17404233
NECOdes <necodes@xxxnoscopemlgxxx.com>
2024-08-31 20:34:32 +0200

    🏝️ initial commit

---
+++ Wezterm/roku.lua
@@ -0,0 +1,34 @@
+local colorscheme = {
+	foreground = '#d3ccc0',
+	background = '#161919',
+
+	cursor_bg = '#d3ccc0',
+	cursor_fg = '#161919',
+	cursor_border = '#d3ccc0',
+
+	selection_fg = '#262c2e',
+	selection_bg = '#f0e9df',
+
+	ansi = {
+		'#161919',
+		'#c95046',
+		'#237b53',
+		'#d9a36c',
+		'#5f819d',
+		'#85678f',
+		'#60a5a5',
+		'#d3ccc0',
+	},
+	brights = {
+		'#324352',
+		'#e27168',
+		'#5bac7d',
+		'#cdaa77',
+		'#81b0d6',
+		'#c58fd6',
+		'#85d3d3',
+		'#f0e9df',
+	},
+}
+
+return colorscheme
---
+++ roku.lua
@@ -0,0 +1,24 @@
+local roku = {
+	color0 = '#161919', -- black (background)
+	color1 = '#c95046', -- red
+	color2 = '#237b53', -- green
+	color3 = '#d9a36c', -- yellow
+	color4 = '#5f819d', -- blue
+	color5 = '#85678f', -- magenta
+	color6 = '#60a5a5', -- cyan
+	color7 = '#d3ccc0', -- white
+	color8 = '#324352', -- bright black
+	color9 = '#e27168', -- bright red
+	color10 = '#5bac7d', -- bright green
+	color11 = '#cdaa77', -- bright yellow
+	color12 = '#81b0d6', -- bright blue
+	color13 = '#c58fd6', -- bright magenta
+	color14 = '#85d3d3', -- bright cyan
+	color15 = '#f0e9df', -- bright white
+}
+
+local roku_extra = {
+	pitch_black = '#050404' -- pitch black
+	dark_black = '#121214' -- dakr black
+	nice_black = '#262c2e' -- nice black
+}
