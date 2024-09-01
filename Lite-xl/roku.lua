9185c2bb7cb00297e3f4d13d8d7aa753c4f31e54
NECOdes <necodes@xxxnoscopemlgxxx.com>
2024-09-01 09:52:59 +0200

    ✍️ Lite-xl config

---
+++ Lite-xl/roku.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#161919" }
+style.background2 = { common.color "#121214" }
+style.background3 = { common.color "#262c2e" }
+style.text = { common.color "#e7c9b3" }
+style.caret = { common.color "#f0f1f1" }
+style.accent = { common.color "#f0f1f1" }
+style.dim = { common.color "#324352" }
+style.divider = { common.color "#151515" }
+style.selection = { common.color "#262c2e" }
+style.line_number = { common.color "#324352" }
+style.line_number2 = { common.color "#f0f1f1" }
+style.line_highlight = { common.color "#324352" }
+style.scrollbar = { common.color "#121214" }
+style.scrollbar2 = { common.color "#050404" }
+
+style.syntax = {}
+style.syntax["normal"] = { common.color "#f0f1f1" }
+style.syntax["symbol"] = { common.color "#c95046" }
+style.syntax["comment"] = { common.color "#324352" }
+style.syntax["keyword"] = { common.color "#85678f" }
+style.syntax["keyword2"] = { common.color "#85678f" }
+style.syntax["number"] = { common.color "#d9a36c" }
+style.syntax["literal"] = { common.color "#f0f1f1" }
+style.syntax["string"] = { common.color "#237b53" }
+style.syntax["operator"] = { common.color "#85678f" }
+style.syntax["function"] = { common.color "#5f819d" }
